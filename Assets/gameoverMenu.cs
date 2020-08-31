1b2c7d395e5d07cdc3497b7cda43c45fa39477e6
matovue <65253920+matovue@users.noreply.github.com>
2020-08-31 11:58:03 +0100

    added gameover words and game broke !!!
    added gameover words and used the playerPrefs to save score

---
+++ Assets/gameoverMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class gameoverMenu : MonoBehaviour
+{
+    public Text highscoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+      highscoreText.text = "HighScore :" + ((int)PlayerPrefs.GetFloat ("HighScore")).ToString();  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

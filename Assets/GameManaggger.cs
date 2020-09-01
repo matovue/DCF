e6123bf787bc1de84410d9c356528516e24b5050
matovue <65253920+matovue@users.noreply.github.com>
2020-09-01 19:54:00 +0100

    First Highscore part that works
    Added to GameManaggger.cs .........public Text highscoreText;
    (IN METHOD public void GameOver() )
    PlayerPrefs.SetFloat ("HighScore", Score.scoreValue); (IN METHOD void Update() ) highscoreText.text = "HighScore : " + ((int)PlayerPrefs.GetFloat ("HighScore")).ToString() + "KB";
    
    UNDER Score.scoreValue += 0;

--- Assets/GameManaggger.cs
+++ Assets/GameManaggger.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManaggger : MonoBehaviour
 {
   public GameObject GameOverCanvas;
-  
-    
+
+  public Text highscoreText; 
 
     void Start()
     {
@@ -23,7 +24,7 @@ public class GameManaggger : MonoBehaviour
         //when character dies the GameOverCanvas is displayed and game is frozen
         GameOverCanvas.SetActive(true);
         Time.timeScale = 0;
-        
+         PlayerPrefs.SetFloat ("HighScore", Score.scoreValue);
         
     }
     // Start is called before the first frame update
@@ -40,6 +41,11 @@ public class GameManaggger : MonoBehaviour
      if (Time.timeScale==1){
           Score.scoreValue += 1;
      }
-     else  Score.scoreValue += 0;   
+     else if(Time.timeScale==0)  
+      {
+          Score.scoreValue += 0;  
+         
+           highscoreText.text = "HighScore : " + ((int)PlayerPrefs.GetFloat ("HighScore")).ToString() + "KB";  
+      } 
     }
 }

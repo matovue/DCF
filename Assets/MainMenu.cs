1519a5c090b695ffe62ff63cea2863889dc04079
matovue <65253920+matovue@users.noreply.github.com>
2020-09-10 13:36:08 +0100

    Play Button Works
    it seems slow though?, on click it takes you to the game

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // To change scenes in unity
+
+public class MainMenu : MonoBehaviour
+{
+ //function we will call whenever we press the play button
+
+ public void PlayGame (){
+// To change scenes in unity
+// Loading next level 
+// In unity we need to add scenes to the queue (file -> build settings -> then drag and drop scenes in the order you want them)
+SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+ }
+}

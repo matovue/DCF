c5f81df5359867cb107189f142bd82a538361a68
matovue <65253920+matovue@users.noreply.github.com>
2020-09-17 01:11:27 +0100

    Solved animation pausing issue in scence when comming back to it from another scene
    (Animationcont.cs) This Script was created for the fire animation using Time.timeScale = 1; to solve the problem of main menu
    // animation pausing when you come back to the scene from another scene

---
+++ Assets/AnimationCont.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This Script was created for the fire animation using Time.timeScale = 1; to solve the problem of main menu 
+// animation pausing when you come back to the scene from another scene
+public class AnimationCont : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      Time.timeScale = 1;    
+    }
+}

f6427654a59728f6bc881515dc24fed8229199f6
matovue <65253920+matovue@users.noreply.github.com>
2020-10-22 13:52:23 +0100

    Added (TAP TO CONTINUE...) message on game

--- Assets/AnimationCont.cs
+++ Assets/AnimationCont.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //This Script was created for the fire animation using Time.timeScale = 1; to solve the problem of main menu 
-// animation pausing when you come back to the scene from another scene
+// fire animation pausing when you come back to the scene from another scene, JUST ADDED THIS SCRIPT TO 
+//(game scene -> fire object) and it worked
 public class AnimationCont : MonoBehaviour
 {
     // Start is called before the first frame update

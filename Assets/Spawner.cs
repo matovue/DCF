8226b342a44b774d6966c80d2ee34144109f6bcf
matovue <65253920+matovue@users.noreply.github.com>
2021-01-02 13:59:37 +0000

    Marvin's help
    In the Spawner.cs made changes in update, when the newpipe is less the -0.5 on the y axis the new position will become 0 on the y axis, but I need to make it the actual height of MKB and a little higher so he can pass under it

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -15,19 +15,30 @@ public class Spawner : MonoBehaviour
     public GameObject pipe;
     public float height;
 
+     public GameObject newpipe;
+
  
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject newpipe = Instantiate(pipe);
-        newpipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
-            
+          newpipe = Instantiate(pipe);
+         newpipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+          
+     
+         
+       
     }
-
+     
     // Update is called once per frame
     void Update()
     {
+          
+        if (newpipe.transform.position.y  < -0.5){
+            
+        newpipe.transform.position = new Vector3(0, 0, 0);
+        print("sucess: " + newpipe.transform.position.y );
+        }
         
         // each time the timer is greater than the max time variable we want to instantiate (create a new of the same thing) our pipe
        // we will modify the height of the pipe slightly by using a randomly generated number ranged between -height and height
@@ -35,12 +46,18 @@ public class Spawner : MonoBehaviour
         {
             GameObject newpipe = Instantiate(pipe);
             newpipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+
+            if (newpipe.transform.position.y  < -0.5){
+            
+                newpipe.transform.position = new Vector3(0, 0, 0);
+        
+            }
             // because we will spawn the pipe infinatley, we can destroy the pipe after some time has passed here its 15
             // 7 as any bigger eg. > 10 causes the spwner to start glitching after some time aroung 6000KB points
             Destroy(newpipe, 7); 
             timer = 0;
             
-         
+         print("TEST: " + newpipe.transform.position.y);  
         }
         
         // after spawning we can set the timer to restart

2b8258f2e45e22ab88ce3eaf34f8baf453bedbaf
matovue <65253920+matovue@users.noreply.github.com>
2021-02-13 16:39:32 +0000

    Created a new spawner5 thats in the middle

---
+++ Assets/Spawner5.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Spawner5 : MonoBehaviour
+{
+    // time we have to wait before new pipe is formed
+    public float maxTime = 1;
+    //Timer to count the time that is passing
+    private float timer = 0;
+    // reference to pipe game object we created in the unity app
+    public GameObject pipe5;
+    public float height;
+
+     public GameObject newpipe;
+
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+         
+       
+    }
+     
+    // Update is called once per frame
+    void Update()
+    {
+          
+        
+        
+        // each time the timer is greater than the max time variable we want to instantiate (create a new of the same thing) our pipe
+       // we will modify the height of the pipe slightly by using a randomly generated number ranged between -height and height
+        if(timer > maxTime)
+        {
+           pipe5.SetActive(true); 
+           GameObject newpipe = Instantiate(pipe5);
+            
+            // because we will spawn the pipe infinatley, we can destroy the pipe after some time has passed here its 15
+            // 7 as any bigger eg. > 10 causes the spwner to start glitching after some time aroung 6000KB points
+            Destroy(newpipe, 7); 
+            timer = 0;
+            
+          
+        }
+        
+        // after spawning we can set the timer to restart
+        timer += Time.deltaTime;
+        
+    }
+}

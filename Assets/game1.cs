a9963e7f457ce10d668f615e37db84275e04d336
matovue <65253920+matovue@users.noreply.github.com>
2021-05-11 19:45:52 +0100

    GameCenter Fix 3rd attempt
    CHANGED GAME1.cs following Auremi code i found refer to game1.cs and gam.cs and delete game2.cs...... Removed the extra button to view score and did it all on one button(SUBMIT/VIEW)

--- Assets/game1.cs
+++ Assets/game1.cs
@@ -10,20 +10,23 @@ using UnityEngine.SceneManagement;
 public class game1 : MonoBehaviour
 {
 
- long scorea; // Gamecenter requires a long variable
+public Transform highscoreText;
+public Transform sendScoreButton;
+ public Transform checkScoreboardButton;
+ private long Highscore; // Gamecenter requires a long variable
  
+    
    public void Who() 
     { 
-          scorea = Score.scoreValue;  
-                Social.ReportScore(scorea,"com.dvf.leaderboard",HighScoreCheck);
-                
+          highscoreText.GetComponent<Text>().text = Highscore.ToString(); 
+                sendScoreButton.GetComponent<Button>().onClick.AddListener(() => { ReportScore(Highscore, "com.dvf.leaderboard");  }); 
+                checkScoreboardButton.GetComponent<Button>().onClick.AddListener(() => {Social.ShowLeaderboardUI();  });
     }
 
-     static void HighScoreCheck(bool result) 
-    {
-        if(result)
-            Debug.Log("score submission successful");
-        else
-            Debug.Log("score submission failed");
-    }
+    void ReportScore (long score, string leaderboardID) {
+    Debug.Log ("Reporting score " + score + " on leaderboard " + leaderboardID);
+    Social.ReportScore (score, leaderboardID, success => {
+	Debug.Log(success ? "Reported score successfully" : "Failed to report score");
+    });
+}
 }

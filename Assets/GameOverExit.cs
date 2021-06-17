c0be739cd82769bf03f15d8ebb1117dccd243c38
matovue <65253920+matovue@users.noreply.github.com>
2021-06-17 01:37:16 +0100

    changes that were reverted

--- Assets/GameOverExit.cs
+++ Assets/GameOverExit.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; // To change scenes in unity
-
+using UnityEngine.Advertisements;
 public class GameOverExit : MonoBehaviour
 {
 
@@ -12,5 +12,17 @@ public class GameOverExit : MonoBehaviour
  SceneManager.LoadScene("Menu");
  // resets the score to zero
  Score.scoreValue = 0;
+
+ 
+      //gameIDs for app store  4028740  for google play 4028741
+      Advertisement.Initialize("4028740", true);
+
+      if (Advertisement.IsReady())  
+          {
+
+       Advertisement.Show();  
  }
+
+
+}
 }
